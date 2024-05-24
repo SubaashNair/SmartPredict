8a4c07ee5d96d6cd33c1ddbe454426e70898557f
Subaash Nair <valiban12@gmail.com>
2024-05-24 20:06:08 +0800

    v0.78

--- smartpredict/explainability.py
+++ smartpredict/explainability.py
@@ -11,9 +11,10 @@ def explain_model(model, X_test, y_test,sample_size=100):
     Explain the model predictions using SHAP.
 
     Parameters:
-    model (estimator): The trained machine learning model.
-    X_test (array-like): Testing features.
-    y_test (array-like): Testing labels.
+    model (estimator): The trained machine learning model. Must have a `predict` method.
+    X_test (array-like or DataFrame): Testing features. This is the dataset used to explain the model's predictions.
+    y_test (array-like or Series): Testing labels. This parameter is currently not used in the function but can be utilized for more advanced explanations or validations.
+    sample_size (int): The number of samples to use for summarizing the background dataset. Reducing the size of the background data can speed up the SHAP calculations.
 
     Returns:
     None
@@ -21,6 +22,11 @@ def explain_model(model, X_test, y_test,sample_size=100):
     # Summarize the background data
     background = shap.sample(X_test, sample_size)
     
+    # Create a KernelExplainer with the summarized background data
     explainer = shap.KernelExplainer(model.predict,background)
+    
+    # Calculate SHAP values for the test dataset
     shap_values = explainer.shap_values(X_test)
+    
+    # Generate a summary plot of the SHAP values
     shap.summary_plot(shap_values, X_test)
